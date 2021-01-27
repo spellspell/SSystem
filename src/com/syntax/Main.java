cf854e7a7f7917551c2071e33ff7c159caa3e3e7
spellspell <spelspel12@gmail.com>
2021-01-27 14:40:01 -0500

    Solar System deployed

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,6 +3,11 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) {
+SolarSystem FactorX = new SolarSystem();
+FactorX.Moon=3;
+FactorX.Sun=1;
+FactorX.Planet=2;
+
 
     }
 }
